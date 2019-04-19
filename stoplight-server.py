33ce2629c5e90fa1687c83c71d07b23d7ea8b1b7
Robert Williams <robert@increscent.org>
2019-04-19 12:59:46 -0600

    updated server

--- stoplight-server.py
+++ stoplight-server.py
@@ -8,40 +8,19 @@ import io
 # compile the serializer
 subprocess.call(["./compile.sh"])
 
-ssid = base64.encodebytes(subprocess.check_output(["./serialize", "35.5", "35.5", "2", "2", "1", "5", "3", "1", "0", "6", "16"]))
+ssid = base64.encodebytes(subprocess.check_output(["./serialize", "35.5", "35.5", "2", "2", "1", "5", "3", "1", "0", "6", "16"])).decode("ascii")
 
-print(ssid.decode("ascii"))
+print(ssid)
 
-ssid_f = open("./.ssid_tmp", "wb")
-ssid_f.write(base64.decodebytes(ssid))
-ssid_f.close()
-
-ssid_f = open("./.ssid_tmp", "rb")
-
-args = subprocess.check_output(["./serialize"], stdin=ssid_f);
-
-ssid_f.close()
-
-print(args.decode("ascii"))
-
-'''
 dev_null = open("/dev/null", "w")
-hostapd_default_conf = open("./hostapd_default.conf", "r")
-default_conf = hostapd_default_conf.read()
+hostapd_default_conf = open("./hostapd_default.conf", "r").read()
 
-#hostapd_bin = subprocess.check_output(["which", "hostapd"])
-
-tick = 0
 while True:
-    tick += 1
     hostapd_conf = open("./hostapd.conf", "w")
-    hostapd_conf.write(default_conf)
-    hostapd_conf.write("\nssid=testing{}\n".format(tick))
+    hostapd_conf.write(hostapd_default_conf)
+    hostapd_conf.write("\n{}\n".format(ssid)
     hostapd_conf.close()
 
     hostapd = subprocess.Popen(["hostapd", "./hostapd.conf"], stdout=dev_null, stderr=dev_null)
-
     time.sleep(1)
-
     hostapd.terminate()
-'''
