2b2c0c8718edf2bf10c4d4060c41afb42ab63240
Robert Williams <robert@increscent.org>
2019-04-19 12:51:58 -0600

    added client program

---
+++ commands.py
@@ -0,0 +1,1 @@
+scan = ["iw", "wlp3s0", "scan", "freq", "2412", "flush", "passive"];
---
+++ stoplight-client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import subprocess
+import time
+import base64
+import io
+import commands
+import re
+
+# compile the serializer
+subprocess.call(["./compile.sh"])
+
+while True:
+    ssid_list = subprocess.check_output(commands.scan).decode("ascii")
+    ssids = list(filter(lambda x: len(x) == 32, re.findall(r'SSID: (.*)\n', ssid_list)))
+
+    for ssid in ssids:
+        ssid_f = open("./.ssid_tmp", "wb")
+        ssid_f.write(base64.decodebytes(ssid))
+        ssid_f.close()
+
+        ssid_f = open("./.ssid_tmp", "rb")
+        args = subprocess.check_output(["./serialize"], stdin=ssid_f);
+        ssid_f.close()
+
+        print(args.decode("ascii"))
