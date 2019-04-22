1daeb9aeaa41e0a1971ebb0e42b678a009ff0b50
Robert Williams <robert@increscent.org>
2019-04-22 06:00:33 +0100

    updated the config

--- stoplight_config.py
+++ stoplight_config.py
@@ -19,6 +19,60 @@ config1 = {
             'red': 9,
             'color': 'red',
             'time': 8
+        },
+        {
+            'direction': 16,
+            'left': False,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'green',
+            'time': 5
+        },
+        {
+            'direction': 24,
+            'left': False,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'red',
+            'time': 8
+        },
+        {
+            'direction': 0,
+            'left': True,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'green',
+            'time': 5
+        },
+        {
+            'direction': 8,
+            'left': True,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'red',
+            'time': 8
+        },
+        {
+            'direction': 16,
+            'left': True,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'green',
+            'time': 5
+        },
+        {
+            'direction': 24,
+            'left': True,
+            'green': 5,
+            'yellow': 2,
+            'red': 9,
+            'color': 'red',
+            'time': 8
         }
     ]
 }
